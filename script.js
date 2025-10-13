5e1563d89e08e65011b9fbde20cfcd3e453ca784
thongster <106802301+thongster@users.noreply.github.com>
2025-10-13 17:55:12 +0700

    added operate function and tested

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// initialize variables
+let numOne = 0;
+let numTwo = 0;
+let operator;
+
+// operate function
+function operate(numOne, operator, numTwo) {
+    let answer;
+    switch (operator) {
+        case "+":
+            answer = add(numOne, numTwo);
+            break;
+        case "-":
+            answer = subtract(numOne, numTwo);
+            break;
+        case "*":
+            answer = multiply(numOne, numTwo);
+            break;
+        case "/":
+            answer = divide(numOne, numTwo);
+            break;
+        default:
+            console.log("invalid operator")
+    };
+    return answer;
+};
+
+operate(18, "*", 2);
